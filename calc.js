b70a051688c135cda5fd63672f8116d8fd1df9e9
Tanja Babic-Debrosse <58664635+tbd007@users.noreply.github.com>
2020-02-03 16:27:56 +0100

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,118 @@
+function test(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+   
+    
+}
+
+function test2(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test2").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+       
+}
+
+function test3(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test3").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+    
+}
+
+function test4(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test4").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+    
+}
+
+function test5(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test5").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+    
+}
+
+function test6(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test6").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+     
+}
+
+function test7(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test7").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+     
+}
+
+function test8(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test8").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+     
+}
+
+function test9(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test9").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+       
+}
+
+function test10(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test10").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+      
+}
+
+function test11(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test11").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+      
+}
+
+function test12(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test12").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+      
+}
+
+function test13(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test13").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+      
+}
+
+function test14(){
+    var summe = document.getElementById("txtResult").innerHTML ;
+    var x=document.getElementById("test14").getAttribute("value");
+    document.getElementById("txtResultItem").innerHTML=x;
+      
+}
+
+function add(){
+    var y=document.getElementById("txtResultItem").innerHTML;
+    var z=document.getElementById("txtResult").innerHTML;
+    document.getElementById("txtResult").innerHTML=Number(y)+ Number(z);
+}
+
+function sub(){
+    var y=document.getElementById("txtResultItem").innerHTML;
+    var z=document.getElementById("txtResult").innerHTML;
+    document.getElementById("txtResult").innerHTML=Number(z)- Number(y);  
+}
+
+function clearentry(){
+    document.getElementById("txtResultItem").innerHTML = 0;
+    document.getElementById("txtResult").innerHTML = 0;  
+}
+    
+
+
